9baef9f3c8000a03baaa975018d19b332a98fcd2
mikekoesumo <mikekoesumo@gmail.com>
2024-10-28 21:19:32 +0700

    Add files via upload

---
+++ Binusmayaquiz.py
@@ -0,0 +1,19 @@
+# DIY Binusmaya LAB Quiz (I can't code, I'll try what I can)
+# define a list
+list<dict> 
+let a = ("lecturer name");
+let b = ("major");
+let c = ("lecturerID");
+a = [Jude, Don, Zhandos, Sera]
+b = [Computer Science]
+c = [xxxxxxxxxx]
+list<class>
+let d = ("classcode");
+d = [L1AC];
+list<schedule>
+let e = ("schedule");
+e = [    ];
+list<dict>
+name(str)
+subject(str)
+ID(str)
